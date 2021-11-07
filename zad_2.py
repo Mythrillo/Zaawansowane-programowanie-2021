6b0a9138fd64f91dd758735c209ee5ce3f26211e
mythrillo <mikolaj.kaluza1999@gmail.com>
2021-11-07 12:56:33 +0100

    Change main.py to zad_2.py

--- zad_2.py
+++ zad_2.py
@@ -1,11 +1,11 @@
 
-# Zadanie 2 a
+# a
 def print_names(list_of_names: list):
     for name in list_of_names:
         print(name)
 
 
-# Zadanie 2 b
+# b
 def multiply_list_elements1(list_of_numbers: list):
     for i in range(len(list_of_numbers)):
         list_of_numbers[i] = list_of_numbers[i] * 2
@@ -16,14 +16,14 @@ def multiply_list_elements2(list_of_numbers: list):
     return [number * 2 for number in list_of_numbers]
 
 
-# Zadanie 3
+# c
 def print_only_even(list_of_numbers: list):
     for number in list_of_numbers:
         if number % 2 == 0:
             print(number)
 
 
-# Zadanie 4
+# d
 def print_every_other(list_of_numbers: list):
     for i in range(0, len(list_of_numbers), 2):
         print(list_of_numbers[i])
